c9358a8ee82b2bd62625f4ce5a376453a32f01a3
Bill Owen <billowen248@gmail.com>
2023-05-06 07:45:22 +1000

    Added base frog script

---
+++ BitJumper/Assets/Scripts/FrogScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Collider2D c2D;
+
+    private PlayerMovement player;
+    [SerializeField] private float agro_distance = 50;
+    [SerializeField] private float jump_strength = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        if (player == null)
+        {
+            throw new Exception("Player not found");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPlayerNear())
+        {
+            //Add check so that it can only jump if on the ground
+            Move();
+        }
+    }
+
+
+    bool IsPlayerNear()
+    {
+        if (    (transform.position.x - player.transform.position.x) * (transform.position.x - player.transform.position.x) +
+                (transform.position.y - player.transform.position.y) * (transform.position.y - player.transform.position.y) <=
+                agroDistance* agroDistance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Move()
+    {
+        int player_direction = transform.position.x - player.transform.position.x > 0 ? -1 : 1;
+        body.velocity = new Vector2(jump_strength * player_direction, jump_strength);
+    }
+
+}
