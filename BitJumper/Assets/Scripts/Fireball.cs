92e2dfed151e2a3e751820cb2d670d82ce46d052
AustinLane994 <auslane03@gmail.com>
2023-05-06 16:51:17 +1000

    Scripts from old project

---
+++ BitJumper/Assets/Scripts/Fireball.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*
+        Collider2D other = collision.collider;
+        if (other.CompareTag("Enemy"))
+        {
+            // Deal damage to the enemy or destroy it
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("Breakable"))
+        {
+            // Deal damage to the enemy or destroy it
+            BreakableObject breakable = other.GetComponent<BreakableObject>();
+            if (breakable != null)
+            {
+                breakable.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+        */
+        Destroy(gameObject);
+    }
+}
