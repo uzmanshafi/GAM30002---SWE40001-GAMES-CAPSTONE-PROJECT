919f0cdf027b44127bf814cf974feaeb8e7e56e7
Uzman Shafi <uzmanshafifassy@gmail.com>
2023-05-09 21:34:49 +1000

    added option to switch between orthographic and perspective view

--- BitJumper/Assets/Scripts/CameraController.cs
+++ BitJumper/Assets/Scripts/CameraController.cs
@@ -5,19 +5,40 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3(2, 1.2f, 0);
+    public Vector3 perspectiveOffset = new Vector3(2, 2f, -6);
+    public Vector3 orthographicOffset = new Vector3(2, 1.2f, 0);
     private Camera cam;
+
+    // Specify the key for switching camera projection mode
+    public KeyCode switchProjectionKey = KeyCode.P;
 
     void Start()
     {
         cam = GetComponent<Camera>();
     }
 
+    void Update()
+    {
+        // Check for the specified key input
+        if (Input.GetKeyDown(switchProjectionKey))
+        {
+            // Toggle between orthographic and perspective projections
+            cam.orthographic = !cam.orthographic;
+        }
+    }
+
     void LateUpdate()
     {
         if (target != null)
         {
-            transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
+            if (cam.orthographic)
+            {
+                transform.position = new Vector3(target.position.x + orthographicOffset.x, target.position.y + orthographicOffset.y, transform.position.z);
+            }
+            else
+            {
+                transform.position = target.position + perspectiveOffset;
+            }
         }
         else
         {
