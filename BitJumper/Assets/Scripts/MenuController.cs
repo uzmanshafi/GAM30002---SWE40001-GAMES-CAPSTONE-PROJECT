dc043d6bab7333acc487773b2f6d5033c736c3e8
Liam O'Brien <liamdobrien99@gmail.com>
2023-05-10 14:08:31 +1000

    Added player preferences
    Added PlayerPrefsEditor package to test applying changes to preferences
    Added ability to save player preferences (currently just volume)
    Adjusted audio back button to return to settings menu not main menu
    Moved PPE and TMP packages to new folder Packages

--- BitJumper/Assets/Scripts/MenuController.cs
+++ BitJumper/Assets/Scripts/MenuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
@@ -10,6 +11,13 @@ public class MenuController : MonoBehaviour
    public string _nameGameLevel;
    private string levelToLoad;
    [SerializeField] private GameObject noSavedGameDialog = null;
+
+   [Header("Volume Settings")]
+   [SerializeField] private TMP_Text volumeTextValue = null;
+   [SerializeField] private Slider volumeSlider = null;
+   [SerializeField] private float defaultVolume = 0.5f;
+
+   [SerializeField] private GameObject confirmationPrompt = null;
 
    public void NewGameDialogYes()
    {
@@ -32,5 +40,36 @@ public class MenuController : MonoBehaviour
    public void ExitButton()
    {
         Application.Quit();
+   }
+
+   public void SetVolume(float volume)
+   {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+   }
+
+   public void ResetButton(string MenuType)
+   {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            ApplyVolume();
+        }
+          
+   }
+
+   public void ApplyVolume()
+   {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+   }
+
+   public IEnumerator ConfirmationBox()
+   {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
    }
 }
