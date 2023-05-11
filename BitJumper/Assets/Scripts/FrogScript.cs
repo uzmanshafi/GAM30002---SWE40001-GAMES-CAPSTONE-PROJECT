32d6f2ec18cada4ff5a01fbc2111d8f01d00e419
Bill Owen <billowen248@gmail.com>
2023-05-12 08:19:47 +1000

    fixed issues

--- BitJumper/Assets/Scripts/FrogScript.cs
+++ BitJumper/Assets/Scripts/FrogScript.cs
@@ -6,17 +6,18 @@ using System;
 public class FrogScript : MonoBehaviour
 {
 
-    private Rigidbody2D rb;
-    private Collider2D c2D;
+    private Rigidbody rb;
 
     private GameObject player;
-    [SerializeField] private float agro_distance = 50;
+    [SerializeField] private float agro_distance = 5000;
     [SerializeField] private float jump_strength = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        //rb = GetComponent<Rigidbody>();
+        
         if (player == null)
         {
             throw new Exception("Player not found");
@@ -37,19 +38,16 @@ public class FrogScript : MonoBehaviour
 
     bool IsPlayerNear()
     {
-        if (    (transform.position.x - player.transform.position.x) * (transform.position.x - player.transform.position.x) +
+        return (    (transform.position.x - player.transform.position.x) * (transform.position.x - player.transform.position.x) +
                 (transform.position.y - player.transform.position.y) * (transform.position.y - player.transform.position.y) <=
-                agro_distance * agro_distance)
-        {
-            return true;
-        }
-        return false;
+                agro_distance * agro_distance);
     }
 
     void Move()
     {
         int player_direction = transform.position.x - player.transform.position.x > 0 ? -1 : 1;
-        rb.velocity = new Vector2(jump_strength * player_direction, jump_strength);
+        transform.position = new Vector3(transform.position.x + player_direction, transform.position.y, transform.position.z);
+        //rb.velocity = new Vector3(jump_strength * player_direction, jump_strength, 0);
     }
 
 }
