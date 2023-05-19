5585a434a851fc3265eafc4c647a8dbb27a7bd37
Bill Owen <billowen248@gmail.com>
2023-05-19 14:32:02 +1000

    Worked on movements

--- BitJumper/Assets/Scripts/FrogScript.cs
+++ BitJumper/Assets/Scripts/FrogScript.cs
@@ -7,10 +7,10 @@ public class FrogScript : MonoBehaviour
 {
 
     private GameObject player;
-
-    private isGroundedClass grounded;
-    [SerializeField] private float agro_distance = 50;
+    private Rigidbody rb;
+    private float agro_distance = 10;
     [SerializeField] private float jump_strength = 10;
+    [SerializeField] private Transform groundCheck;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +19,7 @@ public class FrogScript : MonoBehaviour
         if (player == null) {
             throw new Exception("Player not found");
         }
-        grounded = GetComponent<isGroundedClass>();
-        if (grounded == null) {
-            throw new Exception("grounded not found");
-        }
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -46,12 +43,19 @@ public class FrogScript : MonoBehaviour
     void Move()
     {
         int player_direction = transform.position.x - player.transform.position.x > 0 ? -1 : 1;
+        //divide scale by 2
+        if (Physics.CheckBox(groundCheck.position, groundCheck.localScale / 2, Quaternion.identity, LayerMask.GetMask("Ground"), QueryTriggerInteraction.UseGlobal)) {
 
- 
-        if (grounded.IsGrounded) {
-        Debug.Log("Help");
-            transform.position = new Vector3(transform.position.x + player_direction * Time.deltaTime, 10, transform.position.z);
+            float vx = jump_strength * player_direction;
+            float vy = -(Physics.gravity.y / 2) * (player.transform.position.x - transform.position.x) / vx;
+
+            rb.velocity = new Vector3(vx, vy, 0);
+
         }
+        // if (grounded.IsGrounded) {
+        // Debug.Log("Help");
+        //     transform.position = new Vector3(transform.position.x + player_direction * Time.deltaTime, 10, transform.position.z);
+        // }
         
         //rb.velocity = new Vector3(jump_strength * player_direction, jump_strength, 0);
     }
