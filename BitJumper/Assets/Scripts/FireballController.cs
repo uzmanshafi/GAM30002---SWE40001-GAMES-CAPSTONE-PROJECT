e29e8670f6ffcb1dd85a522f6725423c9e8f37ee
GlitchyRobot <42768914+GlitchyRobot@users.noreply.github.com>
2023-05-19 12:36:38 +1000

    Add New Content

--- BitJumper/Assets/Scripts/FireballController.cs
+++ BitJumper/Assets/Scripts/FireballController.cs
@@ -24,15 +24,26 @@ public class FireballController : MonoBehaviour
 
     private void launchFireball()
     {
+<<<<<<< Updated upstream
         GameObject newFB = Instantiate(fireballPrefab, fireballSP.position, Quaternion.identity);
+=======
+        if (animator != null)
+        {
+            animator.ResetTrigger("Attack");
+            animator.SetTrigger("Attack");
+        }
+        GameObject newFB = Instantiate(fireballPrefab, fireballSP.position, fireballSP.rotation * Quaternion.Euler(0, 0, 90));
+>>>>>>> Stashed changes
         Rigidbody fireballRB = newFB.GetComponent<Rigidbody>();
         if (GetComponent<AdvancePlayerMovement>().IsFacingRight())
         {
             fireballRB.velocity = new Vector3(speed * 1, 0, 0);
+            fireballRB.angularVelocity = new Vector3(speed * 1, 0, 0);
         }
         else
         {
             fireballRB.velocity = new Vector3(speed * -1, 0, 0);
+            fireballRB.angularVelocity = new Vector3(speed * 1, 0, 0);
         }
         Destroy(newFB, 5.0f);
     }
