c6ba2cd50f3db07c5f51e7a720dfc74f5516e31e
AustinLane994 <auslane03@gmail.com>
2023-05-29 11:59:47 +1000

    Using Kinematic equations for jump prediction

--- BitJumper/Assets/Scripts/FrogKingAI.cs
+++ BitJumper/Assets/Scripts/FrogKingAI.cs
@@ -12,7 +12,7 @@ public class FrogKingAI : MonoBehaviour
     [Header("Jump Attack atr")]
     [SerializeField] float jumpAtkHeight;
     [SerializeField] float jumpAtkMS;
-    [SerializeField] float chargeTime;
+    [SerializeField] float chargeTime; //Max charge
 
     [Header("Relative Transforms")]
     [SerializeField] Transform upCheck;
@@ -25,7 +25,7 @@ public class FrogKingAI : MonoBehaviour
     private Rigidbody playerRB;
 
     private bool isCharging = false;
-    private float charging;
+    private float charging; //current charge time
 
     // Start is called before the first frame update
     void Start()
@@ -38,26 +38,30 @@ public class FrogKingAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            chargeJump();
+        }
         if (isCharging)
         {
             if (playerRB.velocity.y != 0)
             {
-                
+                makeJumpPrediction();
             }
             if (charging > chargeTime)
             {
-                
+                makeJumpPrediction();
             }
         }
     }
 
-    private void chargeJump(Vector3 target)
+
+    //private void chargeJump(Vector3 target)
+    private void chargeJump()
     {
         charging = Time.time;
         isCharging = true;
-        Vector3 gravity = Physics.gravity;
-        float initialUpVel = Mathf.Sqrt(-2 * gravity.y * jumpAtkHeight);
-
+        
     }
 
     private void makeJumpPrediction()
@@ -85,8 +89,22 @@ public class FrogKingAI : MonoBehaviour
             }
             points[i] = pos;
         }
-        //return points;
+        jumpAttack(points[points.Length-1]);
 
+    }
+
+    private void jumpAttack(Vector3 target)
+    {
+        Vector3 gravity = Physics.gravity;
+        float dx = target.x - gameObject.transform.position.x;
+        float dy = target.y - gameObject.transform.position.y;
+
+        float initialUpVel = Mathf.Sqrt(-2 * gravity.y * jumpAtkHeight);
+
+        float upwardTime = Mathf.Sqrt((-2 * jumpAtkHeight) / gravity.y);
+        float downwardTime = Mathf.Sqrt((2*(dy - jumpAtkHeight))/ gravity.y);
+
+        //float horizontalDisplacement = 
     }
 
 }
